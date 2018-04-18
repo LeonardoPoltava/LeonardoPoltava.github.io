0d22d68b3d25e12d1891841eef5f471e067fd134
LeonardoPoltava <vladyslavkrp@gmail.com>
2018-04-18 19:24:47 +0300

    desktop commit

--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
 $(document).ready(function () {
+    $('.scroll-pane').jScrollPane({showArrows: false, autoReinitialise: true});
+    $('.career-type').click(function(){
+        var el = $(this).parents('.careers-box').find('.careers-up');
+        if(el.css('display') == 'none') {
+            el.fadeIn();
+            el.css("left", (($(window).width() - el.outerWidth()) / 2) + $(window).scrollLeft() + "px");
+            el.css("top", (($(window).height() - el.outerHeight()) / 2) + $(window).scrollTop() + "px");
+        }
+    });
+    $('.business-btn').click(function(){
+        var el = $('.contact-form-up');
+        if(el.css('display') == 'none') {
+            el.fadeIn();
+            el.css("left", (($(window).width() - el.outerWidth()) / 2) + $(window).scrollLeft() + "px");
+            el.css("top", (($(window).height() - el.outerHeight()) / 2) + $(window).scrollTop() + "px");
+        }
+    });
+    $('.close-up').click(function(){
+        $('.careers-up').fadeOut();
+        $('.contact-form-up').fadeOut();
+    });
 });
