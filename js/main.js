f4e24e8faae92d8d2f0445241055a21ceaed1581
LeonardoPoltava <vladyslavkrp@gmail.com>
2018-04-19 13:23:04 +0300

    adaptive in progress

--- js/main.js
+++ js/main.js
@@ -20,4 +20,27 @@ $(document).ready(function () {
         $('.careers-up').fadeOut();
         $('.contact-form-up').fadeOut();
     });
+    $('.view-workers-btn').click(function(){
+        var el = $(this).parents('.main-right').find('.workers-list');
+        if(el.css('height') == '260px') {
+            el.animate({height: $(el).get(0).scrollHeight}, 1000 );
+            el.addClass('active');
+        }
+        else {
+            el.animate({height: "260px"}, 1000 );
+            el.removeClass('active');
+        }
+    })
+    if($(window).width() < 768) {
+        $('.spotlight-list').slick({
+          dots: false,
+          arrows: false,
+          infinite: true,
+          speed: 1000,
+          slidesToShow: 1,
+          autoplay: false,
+          autoplaySpeed: 5000,
+          pauseOnFocus: false
+        });
+    }
 });
