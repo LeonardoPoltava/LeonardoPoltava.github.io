c1a17fd3c5d18808840ea64008d7f817877d7b0f
LeonardoPoltava <vladyslavkrp@gmail.com>
2018-04-19 15:26:08 +0300

    markup complete

--- js/main.js
+++ js/main.js
@@ -7,13 +7,15 @@ $(document).ready(function () {
             el.css("left", (($(window).width() - el.outerWidth()) / 2) + $(window).scrollLeft() + "px");
             el.css("top", (($(window).height() - el.outerHeight()) / 2) + $(window).scrollTop() + "px");
         }
+        else {
+            el.fadeOut();
+        }
     });
     $('.business-btn').click(function(){
         var el = $('.contact-form-up');
         if(el.css('display') == 'none') {
             el.fadeIn();
             el.css("left", (($(window).width() - el.outerWidth()) / 2) + $(window).scrollLeft() + "px");
-            el.css("top", (($(window).height() - el.outerHeight()) / 2) + $(window).scrollTop() + "px");
         }
     });
     $('.close-up').click(function(){
@@ -25,10 +27,12 @@ $(document).ready(function () {
         if(el.css('height') == '260px') {
             el.animate({height: $(el).get(0).scrollHeight}, 1000 );
             el.addClass('active');
+            this.innerHTML = "Hide All";
         }
         else {
             el.animate({height: "260px"}, 1000 );
             el.removeClass('active');
+            this.innerHTML = "View All";
         }
     })
     if($(window).width() < 768) {
@@ -40,7 +44,8 @@ $(document).ready(function () {
           slidesToShow: 1,
           autoplay: false,
           autoplaySpeed: 5000,
-          pauseOnFocus: false
+          pauseOnFocus: false,
+          adaptiveHeight: true
         });
     }
 });
