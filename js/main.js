2a5a639788d189de7b50dbabcd206bdc09ecaece
LeonardoPoltava <vladyslavkrp@gmail.com>
2018-04-23 19:52:18 +0300

    animation upload

--- js/main.js
+++ js/main.js
@@ -49,7 +49,7 @@ $(document).ready(function () {
         });
     }
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
+        if ($(this).scrollTop() > 250) {
             $('.benefit-box').addClass('changed');
         }
         else {
@@ -57,3 +57,28 @@ $(document).ready(function () {
         }
     });
 });
+var animateHTML = function () {
+  var elems,
+    windowHeight
+  var init = function () {
+    elems = document.getElementsByClassName('hidden')
+    windowHeight = window.innerHeight
+    _addEventHandlers()
+  }
+  var _addEventHandlers = function () {
+    window.addEventListener('scroll', _checkPosition)
+    window.addEventListener('resize', init)
+  }
+  var _checkPosition = function () {
+    for (var i = 0; i < elems.length; i++) {
+      var posFromTop = elems[i].getBoundingClientRect().top
+      if (posFromTop - windowHeight <= 0) {
+        elems[i].className = elems[i].className.replace('hidden', 'animated')
+      }
+    }
+  }
+  return {
+    init: init
+  }
+}
+animateHTML().init()
