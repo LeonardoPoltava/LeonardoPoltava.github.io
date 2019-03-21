a7c3ff50ec43c90964ecdef2f1ac8c5e6444bae1
LeonardoPoltava <vladyslavkrp@gmail.com>
2019-03-21 16:28:05 +0200

    joonko

---
+++ joonko.git/js/main.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+    $('.show-menu').click(function () {
+       var el = $(this).parents('.header-inner').find('.header__right');
+       if(el.css('right') == '-290px') {
+           el.animate({
+               right: "0"
+           }, 1000);
+       }
+       else {
+           el.animate({
+               right: "-290px"
+           }, 1000);
+       }
+    });
+    $('.clients__slider').slick({
+        dots: true,
+        arrows: false,
+        infinite: false,
+        speed: 1000,
+        slidesToShow: 6,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        pauseOnFocus: false,
+        swipeToSlide: true,
+        responsive: [
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            }
+        ]
+    });
+    $('.form__input, .form__area').focus(function() {
+        $(this).parent('div').addClass('active');
+    });
+    $( ".form__select" ).change(function() {
+        $(this).parent('div').addClass('active');
+    })
+    $('.form__input, .form__area').focusout(function() {
+        if($(this).val() == "") {
+            $(this).parent('div').removeClass('active');
+        }
+    });
+    var footHeight = $('footer').outerHeight();
+    $('.wrapper').css('padding-bottom', footHeight);
+    $(window).resize(function() {
+        var footHeight = $('footer').outerHeight();
+        $('.wrapper').css('padding-bottom', footHeight);
+    });
+});
+
