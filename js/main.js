1a20e116715e8ebcb9472b6015afd14dee6308d5
LeonardoPoltava <vladyslavkrp@gmail.com>
2019-05-05 12:41:44 +0300

    joonko1

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    $(".anchor").click(function () {
+        elementClick = $(this).attr("href");
+        destination = $(elementClick).offset().top-0;
+        $("body,html").animate({scrollTop: destination }, 800);
+    });
+
+    $('.section__nav').each(function () {
+        $(this).css('top', 'calc(50% - ' + ($(this).outerHeight() / 2) + 'px)');
+    })
+
+    $('.section__form__box__input').focus(function() {
+        $(this).parent('div').addClass('active');
+    });
+    $('.section__form__box__input').focusout(function() {
+        if($(this).val() == "") {
+            $(this).parent('div').removeClass('active');
+        }
+    });
+    (function($) {
+        $(function() {
+            setTimeout(function() {
+                $('select').styler();
+            }, 100)
+        });
+    })(jQuery);
+});
+
